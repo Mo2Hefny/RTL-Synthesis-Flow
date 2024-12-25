9b8469ffbe8dfbef50a551d742c69fd8bd41de25
Moamen Hefny <moamensaied1@gmail.com>
2024-12-25 05:36:18 +0200

    [refact] Refact booth's algorithms

--- Basic_RTLs/Multipliers/booth_muliplier/booth_multiplier.v
+++ Basic_RTLs/Multipliers/booth_muliplier/booth_multiplier.v
@@ -1,35 +1,32 @@
 module booth_multiplier (
-    input signed [31:0] multiplicand, // The number being multiplied
-    input signed [31:0] multiplier,  // The multiplier
-    output reg signed [63:0] product // The 64-bit product
+    input signed [31:0] a,
+    input signed [31:0] b,
+    output reg signed [63:0] product
 );
-    reg [63:0] A, S, P; // Registers for Booth's operations
-    reg [32:0] Q;       // Extended multiplier with extra bit for Booth encoding
+    reg [63:0] regA, regS, regP;
+    reg [32:0] regQ;
     integer i;
 
     initial begin
-        product = 0; // Initialize product to 0
+        product = 0;
     end
 
     always @(*) begin
-        // Initialize Booth's Registers
-        A = {multiplicand, 32'b0}; // A = Multiplicand shifted left
-        S = {-multiplicand, 32'b0}; // S = -Multiplicand shifted left
-        P = {32'b0, multiplier}; // P = Partial product
-        Q = {multiplier, 1'b0}; // Extended multiplier with an extra 0
+        regA = {a, 32'b0};
+        regS = {-a, 32'b0};
+        regP = {32'b0, b};
+        regQ = {b, 1'b0};
 
-        // Perform Booth's Algorithm
         for (i = 0; i < 32; i = i + 1) begin
-            case (Q[1:0])
-                2'b01: P = P + A; // Add multiplicand to the partial product
-                2'b10: P = P + S; // Subtract multiplicand from the partial product
-                default: P = P;   // No operation
+            case (regQ[1:0])
+                2'b01: regP = regP + regA;
+                2'b10: regP = regP + regS;
+                default: regP = regP;
             endcase
-            P = {P[63], P[63:1]}; // Arithmetic right shift (preserves the sign)
-            Q = {P[0], Q[32:1]};  // Shift the Q register
+            regP = {regP[63], regP[63:1]};
+            regQ = {regP[0], regQ[32:1]};
         end
 
-        // Assign final product
-        product = P;
+        product = regP;
     end
 endmodule
